2ecf3597c72e35c152aaa552695d46f5b8b6313d
jayden <jr4252008@gmail.com>
2025-01-25 19:46:23 -0700

    Revert "Added more conversions"
    This reverts commit f12049a7df1c45a40885164227a53385adafee37.

--- src/main/java/currencyConvert.java
+++ src/main/java/currencyConvert.java
@@ -4,18 +4,9 @@ public class currencyConvert {
         this.val = val;
     }
 
-    public Double convertUSDToCAD(Double val){
+    public Double convertToCAD(Double val){
         return val * 1.43;
         //USD to CAD keeps changing wtf so this is a placeholder
-    }
-    public Double convertUSDToPHP(Double val){
-        return val * 58.25;
-    }
-    public Double convertCADtoUSD(Double val){
-        return val / 1.43;
-    }
-    public Double convertCADtoPHP(Double val){
-        return val * 40.64;
     }
 
 
